dea5b7bcab585272e66bf139fc823d07a0e9d470
chenaoh <darioangellopez38@gmail.com>
2024-01-04 17:01:08 -0300

    formateando

--- src/app.js
+++ src/app.js
@@ -13,7 +13,6 @@ const app = express();
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/sendEmail", mailRouter);
 
