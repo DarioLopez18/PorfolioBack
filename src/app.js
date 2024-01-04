f3b549ce6076781b1e64b444221cdfc3f942c85e
chenaoh <darioangellopez38@gmail.com>
2024-01-04 14:15:31 -0300

    arreglando back

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "*" }));
 
